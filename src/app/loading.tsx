29dd8b0499e15bce59cd74cedc27a1aedc93a432
mr-sazzad <main.sazzad@gmail.com>
2023-12-04 08:53:22 +0600

    some modified in loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="h-full w-full flex justify-center items-center">
-      <div className="border-t-8 border-gray-500 border-solid rounded-full animate-spin w-16 h-16"></div>
-      <p className="mt-4 text-gray-700">Loading...</p>
+      <div className="flex flex-col gap-2">
+        <div className="border-t-4 border-gray-500 border-solid rounded-full animate-spin w-10 h-10"></div>
+        <p className="mt-4 text-gray-700">Loading...</p>
+      </div>
     </div>
   );
 };
